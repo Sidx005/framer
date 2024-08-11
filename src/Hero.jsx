9655dec14813c4e9604260af61a2c3a31041a910
Sidx005 <desaisiddharth1246@gmail.com>
2024-08-11 11:21:44 +0530

    Updated hero sect

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -26,7 +26,7 @@ setOpen(true)
     <motion.div initial={{opacity:0}} animate={{opacity:1}}  exit={{x:'-100vw'}} transition={{delay:.5,duration:1.5,type:'tween'}} className='min-h-[100vh] text-white bg-gradient-to-t from-violet-900 via-black to-black p-5 w-full flex flex-col items-center justify-center'>
 <motion.div drag dragElastic={.5} dragConstraints={{left:0,bottom:0,top:0,right:0}} variants={circle} whileHover='hover' className='h-5 w-5 rounded-full p-5 bg-gradient-to-b from-pink-400 to-violet-800'></motion.div>
     <motion.h1 className='font-bold text-[30px]' initial={{opacity:0,y:-30}} animate={{opacity:1,y:-10}} transition={{delay:1,type:'spring',stiffness:8}}>
-        Hello
+        Hello my name is sid
     </motion.h1>
  <motion.p className='font-bold text-violet-700' onClick={display} initial={{opacity:0}} animate={{opacity:1}} transition={{delay:0.5,duration:2}} style={{cursor:'pointer'}}>   I'm Siddharth</motion.p> 
     <motion.div style={{padding:'2px'}}>   
